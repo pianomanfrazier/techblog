c66879ec12a83d2d37fe8d705655f98ae56e6898
Ryan Frazier <pianomanfrazier@gmail.com>
2017-09-16 23:04:30 -0600

    unpublish post

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,12 @@ echo "building site"
 rm -r public/*
 hugo
 
+echo "cleaning out old site"
+for i in about categories post tags *.html *.png *.xml
+do
+	rm -r $DEPLOY/$i
+done
+
 echo "copying build to $DEPLOY"
 cp -r public/* $DEPLOY
 
